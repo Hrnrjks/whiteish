07d15346a490c79233563a3645c28ced8b78a910
zellar <empty@email>
2019-08-24 12:40:17 +0300

    done

---
+++ whiteish/Form1.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace whiteish
+{
+    partial class form_whiteish
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // form_whiteish
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Name = "form_whiteish";
+            this.Text = "whiteish";
+            this.Load += new System.EventHandler(this.form_whiteish_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ whiteish/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace whiteish
+{
+    public partial class form_whiteish : Form
+    {
+        public form_whiteish()
+        {
+            InitializeComponent();
+        }
+
+        private void form_whiteish_Load(object sender, EventArgs e)
+        {
+            int screenLeft = SystemInformation.VirtualScreen.Left;
+            int screenTop = SystemInformation.VirtualScreen.Top;
+            int screenWidth = SystemInformation.VirtualScreen.Width;
+            int screenHeight = SystemInformation.VirtualScreen.Height;
+
+            this.Size = new System.Drawing.Size(screenWidth + 100, screenHeight + 100);
+            this.Location = new System.Drawing.Point(screenLeft, screenTop);
+        }
+    }
+}
